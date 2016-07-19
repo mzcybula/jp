07180f459668f0bdf7bf619723e484001006a7cd
mzcybula <mzcybula@gmail.com>
2016-07-19 01:18:40 +0100

    task

--- src/test/java/StockTest.java
+++ src/test/java/StockTest.java
@@ -3,6 +3,8 @@ import org.junit.Test;
 import pojo.CommonStock;
 import pojo.Stock;
 
+import static org.junit.Assert.*;
+
 //Test for coverage of equals and hashcode methods
 public class StockTest {
 
@@ -10,14 +12,14 @@ public class StockTest {
     public void testEquals() {
         Stock stock1 = new CommonStock(Stock.Symbol.GIN, 10, 10);
         Stock stock2 = new CommonStock(Stock.Symbol.GIN, 10, 10);
-        Assert.assertEquals(stock1, stock2);
+        assertEquals(stock1, stock2);
     }
 
     @Test
     public void testEquals_WithDiffrentSymbol() {
         Stock stock1 = new CommonStock(Stock.Symbol.GIN, 10, 10);
         Stock stock2 = new CommonStock(Stock.Symbol.ALE, 10, 10);
-        Assert.assertNotEquals(stock1, stock2);
+        assertNotEquals(stock1, stock2);
     }
 
 }
